93a756d9a733727bd1aa0d4db94332f5e0be2692
orbitalquark <70453897+orbitalquark@users.noreply.github.com>
2026-08-30 11:09:39 -0400

    Make use of new `events.UPDATE_UI` updated flag.
    Requires latest Textadept.

--- init.lua
+++ init.lua
@@ -484,9 +484,9 @@ events.connect(events.UPDATE_UI, function(updated)
 end)
 
 -- Highlight differences as text is typed and deleted.
-events.connect(events.MODIFIED, function(_, modification_type)
+events.connect(events.UPDATE_UI, function(updated)
 	if not _VIEWS[view1] or not _VIEWS[view2] then return end
-	if modification_type & (0x01 | 0x02) > 0 then mark_changes() end -- insert text | delete text
+	if updated & buffer.UPDATE_TEXT > 0 then mark_changes() end
 end)
 
 events.connect(events.VIEW_NEW, function()
